9e72a3aa8025ff3fa409f89a8e8c081b95f14764
Yahya <yahiya.jmh@gmail.com>
2024-04-12 21:36:34 -0400

    commit

--- src/UserInputJava.java
+++ src/UserInputJava.java
@@ -2,22 +2,36 @@ import java.util.Scanner; // if userInput in mentioned in the question then u ha
 
 
 
+
 public class UserInputJava {
     public static void main(String[] args) {
-
+        // Write a java program to add two numbers given by user.
         Scanner input = new Scanner(System.in); // an object from Scanner class is created.
 
-        System.out.println("Hello Mr. User pls enter a number:  ");
+        System.out.println("Hello Mr. User pls enter first number:  ");
 
-        int num1=input.nextInt();  // we have taken input from the suer thru keyboard.
+        int num1=input.nextInt();  // we have taken input from the user through the keyboard.
 
-        System.out.println("enter the second number: ");
+        System.out.println("Enter the second number: ");
 
-        int num2=input.nextInt();
+        int num2=input.nextInt(); // we have taken second input also
 
-        int sum=num1+num2;
+        int sum=num1+num2; // we will gonna add num1 and num2 based on user input.
 
-        System.out.println(STR."The sum of 2 given numbers \{num1} and \{num2} is: \{sum}");
+        //System.out.printf("The sum of integers %d and %d is %d",num1,num2,sum);
+        System.out.println("The sum of integers "+num1+ " and "+ num2 +" is "+sum);
+
+        int a=10,b=20,c=30;
+        double d=3.2,e=6.72;
+        char ch='F';
+        String str="Sushil";
+       // System.out.format() and System.out.printf() both are exactly same just the name is different.
+        System.out.format("The values of a,b,c,d,e,ch and str are %d, %d, %d, %.2f, %.2f, %c and %s respectively.",a,b,c,d,e,ch,str);
+
+        // %d for integers
+        // %c for characters
+        // %s for strings
+        // %f for float and double values
 
     }
 }
